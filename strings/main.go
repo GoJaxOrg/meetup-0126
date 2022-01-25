9c563617f5140de3df4cadc4a79a3d2f6819f4cc
runlevl4 <root@runlevl4.com>
2022-01-24 19:27:03 -0500

    Set example up for two approaches for parsing an IP list.

--- strings/main.go
+++ strings/main.go
@@ -3,23 +3,26 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"net/netip"
 	"strconv"
 	"strings"
+	"time"
 )
 
 func main() {
 	var ipList = []string{
 		"10.45.78.90:3343",
+		"127.0.0.1:4000",
 		"123.45.67.89:3306",
 		"78.54.12.63:2345",
 		"250.149.166.81:443",
 		"208.41.196.190:3000",
 		"10.10.38.65",
 		"212.72.170.28:443",
-		"250.66.16.72:3000",
-		"207.38.75.46:23567",
-		"158.24.28.232:30123",
+		"250.66.16.72:30123",
+		"207.38.75.46:8080",
+		"158.24.28.232:26257",
 	}
 
 	var (
@@ -40,13 +43,46 @@ func main() {
 			addrPorts = append(addrPorts, netip.AddrPortFrom(tmpAddr, tmpPort))
 		case true:
 			tmpAddr, _ = netip.ParseAddr(addr)
-			parsedPort, _ := strconv.ParseUint(port, 16, 16)
+			parsedPort, err := strconv.ParseUint(port, 10, 16)
+			if err != nil {
+				fmt.Println(err)
+			}
 			tmpPort = uint16(parsedPort)
 			addrPorts = append(addrPorts, netip.AddrPortFrom(tmpAddr, tmpPort))
 		}
 	}
-
 	for _, addrPort := range addrPorts {
 		fmt.Printf("Type: %T; Data: %v\n", addrPort, addrPort)
+		rs, err := net.DialTimeout("tcp", addrPort.String(), time.Millisecond*50)
+
+		if err != nil {
+			fmt.Println("\terror: ", err)
+			continue
+		}
+		fmt.Printf("\tSuccessfully connected to %s\n", addrPort.String())
+		err = rs.Close()
 	}
+
+	//var addrPorts []netip.AddrPort
+	//
+	//for _, ip := range ipList {
+	//	addrPort, err := netip.ParseAddrPort(ip)
+	//	if err != nil {
+	//		fmt.Printf("error parsing ip address `%s` : %s\n", ip, err)
+	//		continue
+	//	}
+	//	addrPorts = append(addrPorts, addrPort)
+	//}
+	//
+	//for _, addrPort := range addrPorts {
+	//	fmt.Printf("Type: %T; Data: %v\n", addrPort, addrPort)
+	//	rs, err := net.DialTimeout("tcp", addrPort.String(), time.Millisecond*50)
+	//
+	//	if err != nil {
+	//		fmt.Println("\terror: ", err)
+	//		continue
+	//	}
+	//	fmt.Printf("\tSuccessfully connected to %s\n", addrPort.String())
+	//	err = rs.Close()
+	//}
 }
